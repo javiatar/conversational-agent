33e6626ba09aa2320acd5cbbefec11bea64c5aaf
Ricardo Del Rio Fuente <ricardo@malted.ai>
2025-09-28 23:37:22 +0000

    Add some dummy tests and nox session to demonstrate how to do targetted CI

--- noxfile.py
+++ noxfile.py
@@ -7,38 +7,21 @@
 #  ]
 # ///
 
-import argparse
-import os
-from pathlib import Path
-from textwrap import dedent
 
 import nox
 
+PYPROJECT = nox.project.load_toml("pyproject.toml")
 
-@nox.session(venv_backend="uv", default=False, python="3.12")
-def generate_openapi_spec(session: nox.Session):
-    parser = argparse.ArgumentParser(description="Generate an OpenAPI spec")
-    parser.add_argument(
-        "--output",
-        type=Path,
-        help="The file to output the spec to",
-    )
-    args: argparse.Namespace = parser.parse_args(args=session.posargs)
-    output: Path = args.output
-    output.parent.mkdir(parents=True, exist_ok=True)
 
+def uv_sync(session: nox.Session, *params: str):
     uv_env = {"UV_PROJECT_ENVIRONMENT": session.virtualenv.location}
-    session.run_install("uv", "pip", "install", os.getcwd(), env=uv_env)
-    session.run(
-        "python",
-        "-c",
-        dedent(f"""\
-            import json
-            from conversational_agent.core.api.endpoints import app
-            with open("{output}", "w") as fd:
-                json.dump(app().openapi(), fd)
-            """),
-    )
+    session.run("uv", "sync", *params, env=uv_env, silent=True)
+
+
+@nox.session()
+def test(session: nox.Session):
+    uv_sync(session, "--group", "test")
+    session.run("pytest", ".")
 
 
 @nox.session(default=False, python=False)
@@ -64,21 +47,19 @@ def docker_up(session: nox.Session):
     )
 
 
-# @nox.session(default=False, python=False)
-# def docker_down(session: nox.Session):
-#     session.run(
-#         "docker",
-#         "compose",
-#         "-p",
-#         "conversational_agent",
-#         "-f",
-#         "deployment/docker-compose/dev.yml",
-#         "-f",
-#         "deployment/docker-compose/dependencies.yml",
-#         "down",
-#         *session.posargs,
-#         external=True,
-#     )
+@nox.session(default=False, python=False)
+def docker_down(session: nox.Session):
+    session.run(
+        "docker",
+        "compose",
+        "-p",
+        "conversational_agent",
+        "-f",
+        "deployment/docker-compose/application.yml",
+        "down",
+        external=True,
+    )
+
 
 if __name__ == "__main__":
     nox.main()
