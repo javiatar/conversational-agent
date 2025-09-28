1f795ab9c206c189ea56d732193a0261044a528d
Ricardo Del Rio Fuente <ricardo@malted.ai>
2025-09-28 23:37:00 +0000

    Fix infra so i can mount my storage dir and thus all my built indexes

--- src/conversational_agent/utils.py
+++ src/conversational_agent/utils.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 
@@ -13,4 +14,8 @@ def singleton(callable_obj):
     return wrapper
 
 
-STORAGE_PATH = Path(__file__).parent.parent.parent / "storage"
+# Handles both local development and containerized paths
+if os.path.exists("/app/storage"):
+    STORAGE_PATH = Path("/app/storage")  # Only present in container path
+else:
+    STORAGE_PATH = Path(__file__).parent.parent.parent / "storage"  # Local development path
