7d00bbe0876e2d751b170279af17f654c4aaf213
Ricardo Del Rio Fuente <ricardo@malted.ai>
2025-09-29 00:39:10 +0000

    Remove unused kafka config dict

--- src/conversational_agent/config/dependencies/kafka.py
+++
@@ -1,9 +0,0 @@
-"""Stubbed module to set up and configure a Kafka client"""
-
-from pydantic_settings import BaseSettings, SettingsConfigDict
-
-
-class BrokerConnectionConfig(BaseSettings):
-    model_config = SettingsConfigDict(
-        env_prefix="KAFKA__", env_nested_delimiter="__", env_file=".env"
-    )
